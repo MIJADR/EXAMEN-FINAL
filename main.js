64ffc621616c3623b0ad6182369ed69901798775
unknown <mijaildoroteo@gmail.com>
2023-12-11 20:19:28 -0500

    Se agrego el scrip en el html y se comenzo a trabajar en el archivo JS

---
+++ main.js
@@ -0,0 +1,14 @@
+let ubicacionPrincipal = window.pageYOffset; //0
+
+  AOS.init();
+
+window.addEventListener("scroll", function(){
+    let desplazamientoActual = window.pageYOffset; //180
+    if(ubicacionPrincipal >= desplazamientoActual){ // 200 > 180
+        document.getElementsByTagName("nav")[0].style.top = "0px"
+    }else{
+        document.getElementsByTagName("nav")[0].style.top = "-100px"
+    }
+    ubicacionPrincipal= desplazamientoActual; //200
+
+})

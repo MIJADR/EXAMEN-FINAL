6a781ad53eb0b2f8c83187dbe51567af9454acc3
unknown <mijaildoroteo@gmail.com>
2023-12-11 20:22:03 -0500

    Se culmino con el procedimiento de JS

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ window.addEventListener("scroll", function(){
     ubicacionPrincipal= desplazamientoActual; //200
 
 })
+
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
+    if(semaforo){
+        document.querySelectorAll(".hamburguer")[0].style.color ="#fff";
+        semaforo= false;
+    }else{
+        document.querySelectorAll(".hamburguer")[0].style.color ="#000";
+        semaforo= true;
+    }
+
+    enlacesHeader.classList.toggle("menudos")
+})
